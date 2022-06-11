b787af6089d3fd33e16737d5606d29b17c1d8d4d
CollinBradford <collin.p.bradford@gmail.com>
2022-06-11 10:52:34 -0600

    Just adding the code from the Saturday morning meeting!

--- rhsF_1.m
+++ rhsF_1.m
@@ -9,8 +9,9 @@ global x1;
 %dtheta/dt=thetadot
 F(1)=u(2);
 %dthetadot/dt=given function
-F(2)=-k(1)/m(2)*(u(1)-x1(t))+k(2)/m(2)*(u(3)-u(1));
+floor(t * 10000)
+F(2)=-k(1)/m(1)*(u(1)-x1(floor(t*10000)))+k(2)/m(1)*(u(3)-u(1));
 
 F(3)=u(4);
 
-F(4)=-k(2)/m(3)*(u(3)-u(1));
+F(4)=-k(2)/m(2)*(u(3)-u(1));
