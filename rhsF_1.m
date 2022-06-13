0da81bcb2cca7c90a9d56aeea747b94e7eb8e562
Kendall <kjm268@byu.edu>
2022-06-13 18:12:37 -0400

    changed the functions just slightly to make them more accurate

--- rhsF_1.m
+++ rhsF_1.m
@@ -1,3 +1,4 @@
+
 function F=rhsF_1(t,u)
 %pre-allocate memory for f
 F=zeros(length(u),1);
@@ -10,6 +11,6 @@ global tfactor;
 F(1)=u(2);
 F(2)=-k(1)/m(1)*(u(1)-forcingFunction(round(t*tfactor)))+k(2)/m(1)*(u(3)-u(1));
 F(3)=u(4);
-F(4)=-k(2)/m(2)*(u(3)-u(1))+k(3)/m(3)*(u(5)-u(3));
+F(4)=-k(2)/m(2)*(u(3)-u(1))+k(3)/m(2)*(u(5)-u(3));
 F(5)=u(6);
 F(6)=-k(3)/m(3)*(u(5)-u(3));
