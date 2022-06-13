eb810e433850f635f3ef14d325dfb9ad13965c2e
CollinBradford <collin.p.bradford@gmail.com>
2022-06-13 11:46:54 -0600

    Added a third mass to the stystem and did a lot to improve the organization of the code.

--- rhsF_1.m
+++ rhsF_1.m
@@ -4,14 +4,12 @@ F=zeros(length(u),1);
 %global vars needed
 global m;
 global k;
-global x1;
+global forcingFunction;
+global tfactor;
 
-%dtheta/dt=thetadot
 F(1)=u(2);
-%dthetadot/dt=given function
-floor(t * 10000)
-F(2)=-k(1)/m(1)*(u(1)-x1(floor(t*10000)))+k(2)/m(1)*(u(3)-u(1));
-
+F(2)=-k(1)/m(1)*(u(1)-forcingFunction(round(t*tfactor)))+k(2)/m(1)*(u(3)-u(1));
 F(3)=u(4);
-
-F(4)=-k(2)/m(2)*(u(3)-u(1));
+F(4)=-k(2)/m(2)*(u(3)-u(1))+k(3)/m(3)*(u(5)-u(3));
+F(5)=u(6);
+F(6)=-k(3)/m(3)*(u(5)-u(3));
